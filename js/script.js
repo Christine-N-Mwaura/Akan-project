e6fa91ad7f54d30836f0b9f19da64afdde06561b
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-07-15 12:40:38 +0300

    This commit will pick the birth date inputs from the form.

--- js/script.js
+++ js/script.js
@@ -9,27 +9,5 @@ year = dob.slice(2,4);//to pick the last two values of the year input.
 month = document.getElementById('month').value;//get user input
 date = document.getElementById('day').value;
 
-var gender = document.querySelector('input[name = "gender"]:checked').value;//get value of the checked radio button.
 
-
-var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-
-var days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Sunday"];
-
-//This calculation returns a whole number representing the day of the week.
-var dayOfBirth = parseInt(((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10))) % 7;
-
-//control flows to check whether the person is male or female.
-if(gender === "Male"){
-  var mName = maleName[dayOfBirth - 1]// -1 because index numbers begin from 0 and not 1
-  var day = days[dayOfBirth -1]
-  alert(" You were born on a " + day + "." + " Your name is "+ mName);
-    }
-else if (gender === "Female") {
-    var fName = femaleName[dayOfBirth - 1]
-    var day = days[dayOfBirth - 1]
-    alert(" You were born on " + day + "." + " Your name is "+ fName);
-    }
 }
